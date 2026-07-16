b024257cc8c5b9a3c50dd70c1d66624f9b7c925d
cckijf4444-dotcom <cckijf4444@gmail.com>
2026-07-16 17:13:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def identify_plant_with_plantid(image_bytes):
     payload = {
         "images": [base64_image],
         "plant_details": ["common_names"]
-        "language": "zh-tw"
+        "language": "zh"
     }
     
     try:
